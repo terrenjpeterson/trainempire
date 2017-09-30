79538762b79d98d2a13ef728f418fa708af23e65
Terren Peterson <terrenjpeterson@yahoo.com>
2017-09-30 20:54:20 +0000

    updated build pipeline to copy lambda function

--- tools/build.sh
+++ tools/build.sh
@@ -8,5 +8,7 @@ zip -r build.zip index.js package.json node_modules/
 aws s3 cp index.js s3://trainempire/binaries/
 aws s3 cp build.zip s3://trainempire/binaries/
 
+aws lambda update-function-code --function-name trainEmpireGreen --s3-bucket trainempire --s3-key binaries/build.zip
+
 rm build.zip
 
