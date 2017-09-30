2f76a12eb8fc29ad6fd5f6f3961ab4b58deca9bd
Terren Peterson <terrenjpeterson@yahoo.com>
2017-09-30 21:16:33 +0000

    refactored data into separate files

--- tools/build.sh
+++ tools/build.sh
@@ -3,10 +3,11 @@
 
 cd ../src
 
-zip -r build.zip index.js package.json node_modules/
+zip -r build.zip index.js cities.json populations.json package.json connections.json node_modules/
 
 aws s3 cp index.js s3://trainempire/binaries/
 aws s3 cp build.zip s3://trainempire/binaries/
+aws s3 cp connections.json s3://trainempire/binaries/
 
 aws lambda update-function-code --function-name trainEmpireGreen --s3-bucket trainempire --s3-key binaries/build.zip
 
